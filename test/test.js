9ea0c1829bb881725cb32e1bda71b3242bbc5c0f
Victory Osikwemhe <zombieleetnca@gmail.com>
2017-05-09 10:05:38 +0100

    add test for negative formaters

--- test/test.js
+++ test/test.js
@@ -9,21 +9,21 @@ describe("#printf test", () => {
     let successFormater;
     let realFormat;
     beforeEach(() => {
-        
+
         failFormater = "%s %d %f %% %z";
         successFormater = "%s %d %f %%";
         realFormat = successFormater.replace(/%%/g,"");
-        
+
     });
 
     afterEach(() => {
-        
+
         failFormater = undefined;
         successFormater = undefined;
         realFormat = undefined;
     });
 
-    
+
     describe("#handle printf argument", () => {
         it("should throw error if replacementString does not equal format sepcifiers", () => {
             expect(() => control.printf(successFormater))
@@ -36,7 +36,7 @@ describe("#printf test", () => {
             //assert.throws(control.printf({}));
             expect(() => control.printf({})).toThrowError("[object Object] is not a valid string");
         });
-        
+
         it("should succeed if first argument is a string with valid format specifier", () => {
             expect(control.printf(successFormater,"string", 12, 12.5)).toEqual(true);
         });
@@ -64,15 +64,15 @@ describe("#printf test", () => {
     describe("#test for valid formaters", () => {
         describe("test for json formater", () => {
             it("should return true for valid json data", () => {
-                expect(control.printf("%jo", JSON.stringify({}))).toEqual(true);
+                expect(control.printf("%jn", JSON.stringify({}))).toEqual(true);
             });
             it("should throw error for non json types", () => {
-                expect(() => control.printf("%jo", {})).toThrowError("type mismatch at [object Object]");
+                expect(() => control.printf("%jn", {})).toThrowError("type mismatch at [object Object]");
             });
             it("should throw error for invalid json", () => {
-                expect(() => control.printf("%jo", "a")).toThrowError("SyntaxError: Unexpected token a in JSON at position 0");
+                expect(() => control.printf("%jn", "a")).toThrowError("SyntaxError: Unexpected token a in JSON at position 0");
             });
-            
+
         });
 
         describe("test for charcter formater", () => {
@@ -118,8 +118,8 @@ describe("#printf test", () => {
                 expect(() => control.printf("%o", "hi25")).toThrowError(`type mismatch at hi25`);
             });
         });
-        
-        
+
+
     });
 
     describe("#handle modifiers", () => {
@@ -138,7 +138,50 @@ describe("#printf test", () => {
         it("should be truthy when modifier is less than replacementString length", () => {
             expect(control.printf("%4.2s","hello")).toBeTruthy();
         });
+        it("should work if spaceAmount is not specifed", () => {
+            expect(control.printf("%.3d", 21)).toBeTruthy();
+        });
+        it("should print expontential numbers with a capital letter E", () => {
+            expect(control.printf("%E", 2.2)).toBeTruthy();
+        });
+        it("should be truthy when coverting string to binary numbers", () => {
+            expect(control.printf("%bi", "victory")).toBeTruthy();
+        });
+        it("should be truthy when coverting decimal numbers to binary numbers", () => {
+            expect(control.printf("%bi", 21)).toBeTruthy();
+        });
+        it("should be truthy when computing number precision", () => {
+            expect(control.printf("%.4f", 21.55678)).toBeTruthy();
+        });
+        it("should be truthy when computing decimal places for o" , () => {
+            expect(control.printf("%.3o", 21)).toBeTruthy();
+        });
+        it("should be truthy when computing for the amount of data to print in a javascript object", () => {
+            expect(control.printf("%.3ob", {js:"javascript", py:"python", rb:"ruby", pl:"perl", sh: "bash"})).toBeTruthy();
+        });
+        it("should be truthy when computing for amount of data to print in JSON", () => {
+            expect(control.printf("%.2jn", JSON.stringify({a:"a", b:"b", c:"c", d:"d", f:"f"}))).toBeTruthy();
+        });
+        it("should be truthy when computing for amount of data to print in an array", () => {
+            expect(control.printf("%.2ar", ["a","b","c","d"])).toBeTruthy();
+        });
+        it("should be truthy when computing for exponential values with a small letter E", () => {
+            expect(control.printf("%.3e", 2.2)).toBeTruthy();
+        });
+        it("should be truthy when computing for exponential values with a capital letter E", () => {
+            expect(control.printf("%.3E", 2.2)).toBeTruthy();
+        });
+        it("should be truthy when calculating the number of characters to print", () => {
+            expect(control.printf("%.3c","victory")).toBeTruthy();
+        });
+        it("should be truthy when coverting string to binary numbers", () => {
+            expect(control.printf("%.3bi", "victory")).toBeTruthy();
+        });
+        it("should be truthy when the octal formaters has a negative modifier", () => {
+            expect(control.printf("%.-3o", 32)).toBeTruthy();
+        });
+        it("should be truthy when any other modifier has a negative formater", () => {
+            expect(control.printf("%1.-3s", "hi")).toBeTruthy();
+        });
     });
 });
-
-
